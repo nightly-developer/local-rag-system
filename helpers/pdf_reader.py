16074145c67a136afad2dfb501d8e758b971f8d6
Vaibhav Kesarkar <jarvice.robert@gmail.com>
2024-10-10 00:12:41 +0530

    added pdf reading functionality

---
+++ helpers/pdf_reader.py
@@ -0,0 +1,22 @@
+from splitter import split_document
+from langchain_community.document_loaders import PyPDFLoader
+from langchain.docstore.document import Document
+
+async def load_pdf(pdf_file):
+  # Initialize the PDF loader inside the function
+  loader = PyPDFLoader(
+    pdf_file,
+    extract_images=True,
+    headers=None,
+    extraction_mode="plain",
+  )
+  pacge_count = 1
+  async for page in loader.alazy_load():
+    document = Document(
+      metadata=page.metadata,
+      page_content=page.page_content
+    )
+    print("page:",pacge_count)
+    pacge_count += 1
+
+    yield await split_document(document) # Yield each chunk one at a time 
